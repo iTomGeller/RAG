dd181d6a0436268e8dc02313e1680ba662880a82
riy <3141773287@qq.com>
2025-07-05 09:29:53 +0800

    Feedback beautify

--- RAG/src/components/Siderbar/FeedbackDrawer.vue
+++ RAG/src/components/Siderbar/FeedbackDrawer.vue
@@ -14,24 +14,28 @@
             type="textarea"
             v-model="feedbackText"
             :placeholder="t('feedback.placeholder')"
-            :autosize="{ minRows: 5, maxRows: 10 }"
+            :autosize="{ minRows: 15, maxRows: 20 }"
           />
         </el-form-item>
-        <el-form-item >
-          <el-button type="primary" @click="submitFeedback">{{ t('feedback.submit') }}</el-button>
-          <el-button @click="closeDrawer">{{ t('feedback.cancel') }}</el-button>
-        </el-form-item>
+        <!-- <el-form-item > -->
+          <div class = 'btn-group'>
+          <el-button @click="submitFeedback" class="btn btn-primary" >{{ t('feedback.submit') }}</el-button>
+          <el-button @click="closeDrawer" class="btn btn-secondary">{{ t('feedback.cancel') }}</el-button>
+          </div>  
+        <!-- </el-form-item> -->
       </el-form>
     </div>
 
+    <el-divider></el-divider>
+
     <el-button @click="handleClick">最近反馈</el-button>
-    <RecentFeedback v-model="isDialogVisible"/>
+    <RecentFeedback v-model="isDialogVisible" ref="feedbackRef"/>
     
   </el-drawer>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import FeedbackService from '@/service/FeedbackService';
 import RecentFeedback from './RecentFeedback.vue';
@@ -47,9 +51,19 @@ const feedbackText = ref('');
 
 const isDialogVisible = ref(false);
 
-const handleClick = () => {
+const feedbackRef = ref(null);//instance of RecentFeedback
+
+
+const handleClick = async() => {
   isDialogVisible.value = true;
+
+  await nextTick();
+  
+  if (feedbackRef.value){
+    feedbackRef.value.refresh();
+  }
 }
+
 
 const submitFeedback = async () => {
   if (!feedbackText.value.trim()) {
@@ -80,5 +94,28 @@ const closeDrawer = () => {
   padding: 16px;
 }
 
+.btn-group {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  }
 
+.btn-group .btn{
+  border-radius: 10px;
+  color: #282828;
+  /* background-color: #e2e6eb; */
+  cursor: pointer;
+}
+
+.btn:hover{
+    background-color: #d5d8dc;
+}
+
+.btn-primary{
+  background-color: #e2e6eb;
+    }
+
+.btn-secondary{
+    background-color: #ffffff;
+    }
 </style>
